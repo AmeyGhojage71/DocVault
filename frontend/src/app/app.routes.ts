3c5b1dcd1e377490e03a401032ac36cf473a1594
Pallu <gandhakte pallavi>
2026-02-17 18:47:05 +0530

    created folders

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Upload } from './upload/upload';
 import { DocumentList } from './document-list/document-list';
 
 export const routes: Routes = [
-  { path: '', component: Upload  },
+  { path: '', component: Upload },
   { path: 'documents', component: DocumentList }
 ];
--- frontend/src/app/document-list/document-list.ts
+++ frontend/src/app/document-list/document-list.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-document-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './document-list.html',
-  styleUrl: './document-list.css',
+  styleUrl: './document-list.css'
 })
-export class DocumentList {
+export class DocumentList implements OnInit {
 
+  docs: any[] = [];
+
+  ngOnInit() {
+    fetch('https://localhost:5001/api/documents')
+      .then(res => res.json())
+      .then(data => {
+        this.docs = data;
+      });
+  }
 }
