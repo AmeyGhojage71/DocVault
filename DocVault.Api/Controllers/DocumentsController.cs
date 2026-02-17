b0959bfa69693ccbec4f894dfb7bd44e1b9448e1
Amey Anil Khot <ameykhot2612@gmail.com>
2026-02-17 18:10:40 +0530

    feat(api): add GET /api/documents to list documents from CosmosDB

--- DocVault.Api/Controllers/DocumentsController.cs
+++ DocVault.Api/Controllers/DocumentsController.cs
@@ -1,3 +1,13 @@
 using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos;
+[HttpGet]
+public async Task<IActionResult> List()
+{
+    var query = "SELECT * FROM c";
+    var iterator = _cosmosContainer.GetItemQueryIterator<dynamic>(query);
+    var results = new List<dynamic>();
+    while (iterator.HasMoreResults)
+        results.AddRange(await iterator.ReadNextAsync());
+    return Ok(results);
+}
