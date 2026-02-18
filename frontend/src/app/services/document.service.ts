604f64d735c2eaaaccb5d811f6cef6f3f6ef5f0f
Amey Anil Khot <ameykhot2612@gmail.com>
2026-02-18 14:33:42 +0530

    UI integration

---
+++ frontend/src/app/services/document.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface DocRecord {
+    id: string;
+    fileName: string;
+    url: string;
+    uploadedOn: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class DocumentService {
+    private readonly api = 'http://localhost:5032/api/documents';
+
+    constructor(private http: HttpClient) { }
+
+    upload(file: File): Observable<DocRecord> {
+        const form = new FormData();
+        form.append('file', file, file.name);
+        return this.http.post<DocRecord>(this.api, form);
+    }
+
+    list(): Observable<DocRecord[]> {
+        return this.http.get<DocRecord[]>(this.api);
+    }
+}
